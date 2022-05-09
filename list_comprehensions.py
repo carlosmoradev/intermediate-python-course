684220bede669c3f71d6ed4abc02e3c15842cc0f
Carlos Mora <git@carlosmora.dev>
2022-05-09 09:16:36 -0500

    Solucion por medio de list comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,9 +1,12 @@
 #Crear una lista con los cuadrados de los 100 primeros naturales no divisibles por 3
 def run():
-    squares = []
-    for i in range(1, 101):
-        if i % 3 != 0:
-            squares.append(i**2)
+    # squares = []
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         squares.append(i**2)
+
+
+    squares = [i**2 for i in range(1, 101) if i % 3 != 0]
 
     print(squares)
 
